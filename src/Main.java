c4bf04455fa015fd12536be601f22d5a1a103686
Talakh <talax98@gmail.com>
2017-06-30 17:25:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,18 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws ParseException{
-        CollectionJournal arrayJournal = new CollectionJournal();
-        ArrayJournal betta = new ArrayJournal();
+        CollectionJournal collectionJournal = new CollectionJournal();
+        ArrayJournal arrayJournal = new ArrayJournal();
         Record record = new Record(new Date(119,1,1,12,12,12), Importance.Recovery_Normal,"1","asdf");
         Record record1 = new Record(new Date(110,1,1,12,12,12), Importance.Recovery_Normal,"1","qwer");
-        betta.add(record);
-        betta.add(record1);
+        arrayJournal.add(record);
+        arrayJournal.add(record1);
         System.out.println();
-        arrayJournal.add(betta);
-        arrayJournal.printRecords();
-        arrayJournal.sortByDate();
+        collectionJournal.add(arrayJournal);
+        collectionJournal.printRecords();
+        collectionJournal.sortByDate();
         System.out.println();
-        arrayJournal.printRecords();
+        collectionJournal.printRecords();
         System.out.println();
     }
 }
